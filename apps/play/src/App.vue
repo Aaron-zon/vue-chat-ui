cbdd9ead2f7ba11bf7c2b554b01c3cee6e330006
dl_zhoujunyang <1640485761@qq.com>
2025-04-07 18:57:10 +0900

    init

---
+++ apps/play/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { add } from '@vue-chat-ui/hooks'
+console.log(add(1, 9))
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- play/index.js
+++
@@ -1,3 +0,0 @@
-import {add} from '@vue-chat-ui/hooks'
-
-console.log(add(1, 9))
